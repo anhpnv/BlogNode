4df0a7bd376149c05be1de9be9dcf0883e92f122
Viet Anh <phamvietanh0978442493@gmail.com>
2020-05-24 12:43:38 +0700

    lowDB

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express')
+const db = require('./db')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-var users = [
-    {id: 1,name: 'Viet Anh'},
-    {id: 2,name: 'Tung'},
-    {id: 3,name: 'Tu'},
-]
-
 app.set('view engine','pug')
 app.set('views','./views')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', function(req,res){
     res.render('index',{
@@ -19,17 +18,24 @@ app.get('/', function(req,res){
 
 app.get('/users',(req,res) => {
     res.render('users/index',{
-        name: users
+        name: db.get("users").value()
     })
 })
 
 app.get('/users/search',function(req,res){
     var q = req.query.q
-    var matchedUsers = users.filter(function(user){
+    var matchedUsers = db.get("users").value().filter(function(user){
         return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1
     })
     res.render('users/index',{
         name:matchedUsers
     })
+})
+app.get('/users/create', function(req, res){
+    res.render('users/create');
+})
+app.post('/users/create', function(req,res){
+    db.get('users').push(req.body).write()
+    res.redirect('/users')
 })
 app.listen(port, () => console.log("Example app listening on port " + port))
