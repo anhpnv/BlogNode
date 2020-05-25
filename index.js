cf2d8409caffc49af4e720d16494d623066b0897
Viet Anh <phamvietanh0978442493@gmail.com>
2020-05-25 20:51:29 +0700

    Add users:id

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const db = require('./db')
 const bodyParser = require('body-parser')
+const shortid = require('shortid')
 const app = express()
 const port = 3000
 
@@ -35,7 +36,16 @@ app.get('/users/create', function(req, res){
     res.render('users/create');
 })
 app.post('/users/create', function(req,res){
+    req.body.id = shortid.generate()
     db.get('users').push(req.body).write()
     res.redirect('/users')
+})
+
+app.get('/users/:id',function(req,res){
+    var id = req.params.id;
+    var user = db.get('users').find({id}).value()
+    res.render('users/view',{
+        user
+    })
 })
 app.listen(port, () => console.log("Example app listening on port " + port))
