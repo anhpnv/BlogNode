8d0292dd697c73b61e1a8154379ca2df69829abc
Viet Anh <phamvietanh0978442493@gmail.com>
2020-05-25 21:23:56 +0700

    controller

---
+++ controller/user.controller.js
@@ -0,0 +1,33 @@
+const db = require('../db')
+const shortid = require('shortid')
+
+module.exports.index = (req,res) => {
+    res.render('users/index',{
+        name: db.get("users").value()
+    })
+}
+module.exports.search = function(req,res){
+    var q = req.query.q
+    var matchedUsers = db.get("users").value().filter(function(user){
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1
+    })
+    res.render('users/index',{
+        name:matchedUsers
+    })
+}
+
+module.exports.getUser = function(req, res){
+    res.render('users/create');
+}
+module.exports.postUser = function(req,res){
+    req.body.id = shortid.generate()
+    db.get('users').push(req.body).write()
+    res.redirect('/users')
+}
+module.exports.id = function(req,res){
+    var id = req.params.id;
+    var user = db.get('users').find({id}).value()
+    res.render('users/view',{
+        user
+    })
+}
