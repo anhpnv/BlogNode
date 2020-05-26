a4ed31b62475f2f496cbfd50248c7479ea59d11d
Viet Anh <phamvietanh0978442493@gmail.com>
2020-05-27 02:38:40 +0700

    Validation Folder

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,13 +1,13 @@
 var express = require('express')
-
 var controller = require('../controller/user.controller')
+var validate = require('../validate/user.validate')
 var router = express.Router()
 
 router.get('/', controller.index)
 
 router.get('/search', controller.search)
 router.get('/create', controller.getUser)
-router.post('/create', controller.postUser)
+router.post('/create', validate.postCreate, controller.postUser)
 
 router.get('/:id', controller.id)
 
