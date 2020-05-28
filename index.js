c48486681c3a8a655238b89df9a57f8c780c13ee
Viet Anh <phamvietanh0978442493@gmail.com>
2020-05-28 17:04:23 +0700

    add dotenv

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express')
 const bodyParser = require('body-parser')
 const userRoute = require('./routes/user.route')
@@ -12,7 +13,7 @@ app.set('views','./views')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(cookieParser('abcd123456'))
+app.use(cookieParser(process.env.SESSION_SECRET))
 app.use(express.static('public'))
 app.get('/', function(req,res){
     res.render('index',{
