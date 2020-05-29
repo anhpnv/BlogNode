51c7f0374a82e21ad3deaf4d585be57c24ba9dd5
Viet Anh <phamvietanh0978442493@gmail.com>
2020-05-29 13:30:41 +0700

    Add session middleware

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const cookieParser = require('cookie-parser')
 const userRoute = require('./routes/user.route')
 const authRoute = require('./routes/auth.route')
 const productRoute = require('./routes/product.route')
+const cartRoute = require('./routes/cart.route')
 const authMiddleware = require('./middlewares/auth.middlewares')
+const sessionMiddleware = require('./middlewares/sessions.middlewares')
 
 const app = express()
 const port = 3000
@@ -17,12 +19,14 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cookieParser(process.env.SESSION_SECRET))
 app.use(express.static('public'))
+app.use(sessionMiddleware)
 app.get('/', function(req,res){
     res.render('index',{
         name:'AAA'
     })
 })
-app.use('/users',authMiddleware.requireAuth,userRoute)
-app.use('/auth',authRoute)
-app.use('/product', productRoute)
+app.use('/users',authMiddleware.requireAuth,userRoute);
+app.use('/auth',authRoute);
+app.use('/products', productRoute);
+app.use('/cart', cartRoute)
 app.listen(port, () => console.log("Example app listening on port " + port + ".If you want to debug, you can use --inspect in npm start"))
