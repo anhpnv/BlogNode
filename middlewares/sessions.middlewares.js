50d4207aac0d01646d39934fcc411c9f3b824a3c
Viet Anh <phamvietanh0978442493@gmail.com>
2020-06-08 15:33:29 +0700

    Session middlewares

--- middlewares/sessions.middlewares.js
+++ middlewares/sessions.middlewares.js
@@ -1,14 +1,17 @@
 var db = require('../db')
 var shortId = require('shortid')
+var Session = require('../models/session.model')
 module.exports = function(req, res, next){
     if(!req.signedCookies.sessionId){
         var sessionId = shortId.generate()
         res.cookie('sessionId', sessionId,{
             signed:true,
         });
-        db.get('sessions').push({
-            id: sessionId
-        }).write()
+        var newSession = new Session({id:sessionId})
+        newSession.save(function(err,result){
+            if(err) return console.error(err)
+            console.log("Insert Session successfully")
+        })
     }
     next()
 }
